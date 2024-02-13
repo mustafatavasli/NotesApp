9b8cfbb605c23280068d821da9e9af651a44dcfb
mustafatavasli <musstafatavasli@gmail.com>
2024-02-13 23:23:01 +0300

    ContentView Changed, DetailView Added

---
+++ Notes Watch App/View/DetailView.swift
@@ -0,0 +1,64 @@
+//
+//  DetailView.swift
+//  Notes Watch App
+//
+//  Created by Mustafa TAVASLI on 13.02.2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    // MARK: Property
+    
+    // Notun kendisi
+    let note : Note
+    // Toplam not sayisi
+    let count : Int
+    // Notun konumunu gosterecek index degeri
+    let index : Int
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 3) {
+            
+            // Header
+            HStack {
+                Capsule()
+                    .frame(height: 1)
+                Image(systemName: "note.text")
+                Capsule()
+                    .frame(height: 1)
+            }
+            .foregroundColor(.accentColor)
+            // Content
+            Spacer()
+            ScrollView(.vertical) {
+                Text(note.text)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+            }
+            Spacer()
+            // Footer
+            HStack(alignment: .center) {
+                Image(systemName: "gear")
+                    .imageScale(.large)
+                Spacer()
+                Text("\(count) / \(index + 1)")
+                Spacer()
+                Image(systemName: "info.circle")
+                    .imageScale(.large)
+            }
+            .foregroundColor(.secondary)
+        }
+        .padding(3)
+    }
+}
+
+struct DetailView_Previews : PreviewProvider {
+    static var sampleData : Note = Note(id: UUID(), text: "watchOS App")
+    
+    static var previews: some View {
+        DetailView(note: sampleData, count: 5, index: 1)
+    }
+}
